88c6bdab6be0e08dbb194b6377f9d52b2c4ed0b1
SahilHanda300 <109428092+SahilHanda300@users.noreply.github.com>
2023-10-08 10:45:27 +0530

    Files Added : main.py & ScrapedData.csv
    This project uses BeautifulSoup which scrapes data from amazon with search query : Samsung 
    This saves the top 10 title and it's price in the CSV file using Pandas .
    
    Install BeautifulSoup and Pandas to Run It.
    
    For Documrntation :
    BeautifulSoup : https://pypi.org/project/beautifulsoup4/#:~:text=Beautiful%20Soup%20is%20a%20library,and%20modifying%20the%20parse%20tree.
    Pandas : https://pandas.pydata.org/

---
+++ main.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# Headers used for preventing Server Error
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+r = requests.get("https://www.amazon.in/s?k=samsung+mobile&crid=2K4C4LAIP0K9O&sprefix=samsu%2Caps%2C192&ref=nb_sb_ss_ts-doa-p_2_5",headers=headers)
+
+soup = BeautifulSoup(r.text,"html.parser")
+
+title = soup.select("span.a-size-medium.a-color-base.a-text-normal")
+
+price = soup.select("span.a-price-whole")
+
+
+data={"Product":[],"Price":[]}
+
+
+# It will show the top 10 recommendations of Title
+# and Product by following an incrementer from the
+# top :)
+
+
+title_length=0
+for titles in title:
+    title_length+=1
+    data["Product"].append(titles.string)
+    if title_length==10:
+        break
+
+
+price_length = 0
+for prices in price:
+    price_length+=1
+    data["Price"].append(prices.string)
+    if price_length==10:
+        break
+
+df = pd.DataFrame.from_dict(data)
+
+df.to_csv("ScrapedData.csv",index=False)
+
+# Link Used :
+
+# https://www.amazon.in/s?k=samsung+mobile&crid=2K4C4LAIP0K9O&sprefix=samsu%2Caps%2C192&ref=nb_sb_ss_ts-doa-p_2_5
+
+# The Data generated in .csv right niw is generated as per
+# date of 08-10-2023
